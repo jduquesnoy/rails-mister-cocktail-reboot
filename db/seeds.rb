6d2fa51a42d7f61a23a41b34041ffcc8689aed81
Jocelyn Duquesnoy <jocelyn.duquesnoy@yahoo.fr>
2020-09-18 21:46:00 +0200

    parsing json url

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,25 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 require 'json'
 require 'open-uri'
-
+puts "Create ingredients"
 url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
-user_serialized = open(url).read
-user = JSON.parse(user_serialized)
+ingredient_serialized = open(url).read
+ingredient = JSON.parse(ingredient_serialized)
+ingredient["drinks"].each do |ingredient|
+  i =Ingredient.create(name: ingredient["strIngredient1"])
+  put "create #{i.name}"i
+end
+
+# puts "Destroy ingredients"
+# Ingredient.destroy_all if Rails.env.development?
+
+# puts "Destroy Cocktails"
+# Cocktail.destroy_all if Rails.env.development?
+
+# puts "Create ingredients"
+# url = "https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
+# ingredients = JSON.parse(open(url).read)
+# ingredients["drinks"].each do |ingredient|
+#   i = Ingredient.create(name: ingredient["strIngredient1"])
+#   puts "create #{i.name}"i
+# end
